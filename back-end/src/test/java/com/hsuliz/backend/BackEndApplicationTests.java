f8ee2c51312059ec0b9882a749030ce6f9105c05
mymytre <sasha.suliz@gmail.com>
2022-11-06 17:47:17 +0100

    Init

---
+++ back-end/src/test/java/com/hsuliz/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsuliz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
