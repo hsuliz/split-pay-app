7f0e1ba406a36030150cf882d0a430bb0e858548
mymytre <sasha.suliz@gmail.com>
2022-11-08 12:17:16 +0100

    Basic login thing

--- front-end/src/components/AddClient.tsx
+++ front-end/src/components/AddClient.tsx
@@ -5,8 +5,7 @@ import clientService from "../services/clientService";
 const AddClient: React.FC = () => {
 
     const initialTutorialState = {
-        name: "",
-        email: ""
+        name: "", email: ""
     }
 
     const [client, setClient] = useState<Client>(initialTutorialState);
@@ -20,23 +19,22 @@ const AddClient: React.FC = () => {
     const saveTutorial = () => {
 
         const data = {
-            name: client.name,
-            email: client.email
-        };
+            name: client.name, email: client.email
+        }
 
         clientService.postClient(data)
             .then((response: any) => {
                 setClient({
-                    name: response.data.name,
-                    email: response.data.email,
+                    name: response.data.name, email: response.data.email,
                 });
                 setSubmitted(true);
                 console.log("success");
             })
             .catch((e: Error) => {
                 console.log(e);
-            });
-    };
+            })
+
+    }
 
     const newTutorial = () => {
         setClient(initialTutorialState);
@@ -45,51 +43,46 @@ const AddClient: React.FC = () => {
 
     return (
         <div className="submit-form was-validated">
-            {submitted ? (
-                <div>
-                    <h4>You submitted successfully!</h4>
-                    <button className="btn btn-success" onClick={newTutorial}>
-                        Add
-                    </button>
+            {submitted ? (<div>
+                <h4>You submitted successfully!</h4>
+                <button className="btn btn-success" onClick={newTutorial}>
+                    Add
+                </button>
+            </div>) : (<div>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        id="name"
+                        type="text"
+                        required
+                        className="form-control"
+                        value={client.name}
+                        onChange={handleInputChange}
+                        name="name"
+                        placeholder="Enter a name"
+                    />
                 </div>
-            ) : (
-                <div>
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
-                        <input
-                            id="name"
-                            type="text"
-                            required
-                            className="form-control"
-                            value={client.name}
-                            onChange={handleInputChange}
-                            name="name"
-                            placeholder="Enter a name"
-                        />
-                    </div>
 
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            id="email"
-                            type="email"
-                            required
-                            className="form-control"
-                            value={client.email}
-                            onChange={handleInputChange}
-                            name="email"
-                            placeholder="Enter a email"
-                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                        />
-                    </div>
-
-                    <button onClick={saveTutorial} className="btn btn-success">
-                        Submit
-                    </button>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        required
+                        className="form-control"
+                        value={client.email}
+                        onChange={handleInputChange}
+                        name="email"
+                        placeholder="Enter a email"
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                    />
                 </div>
-            )}
-        </div>
-    );
+
+                <button onClick={saveTutorial} className="btn btn-success">
+                    Submit
+                </button>
+            </div>)}
+        </div>);
 
 }
 
