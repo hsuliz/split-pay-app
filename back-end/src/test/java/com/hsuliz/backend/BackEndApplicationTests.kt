ebe6999fee26f9560de2541e7e945ca1cee6317e
mymytre <sasha.suliz@gmail.com>
2022-11-21 20:07:27 +0100

    BackEndApplicationTests

--- back-end/src/test/java/com/hsuliz/backend/BackEndApplicationTests.kt
+++ back-end/src/test/java/com/hsuliz/backend/BackEndApplicationTests.kt
@@ -1,13 +1,42 @@
 package com.hsuliz.backend
 
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.web.client.RestTemplate
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 internal class BackEndApplicationTests {
 
-    @Disabled
-    fun contextLoads() {
+    @LocalServerPort
+    private var port: Int? = null
+
+    private var baseUrl: String = "http://localhost"
+
+
+    companion object {
+
+        private lateinit var restTemplate: RestTemplate
+
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll(): Unit {
+            restTemplate = RestTemplate()
+        }
+
+    }
+
+    //http://localhost:8080/api/auth/register
+    @BeforeEach
+    fun beforeEach() {
+        baseUrl = "$baseUrl:$port/api"
+    }
+
+    @Test
+    fun `dude`() {
+        print(baseUrl)
     }
 
 }
