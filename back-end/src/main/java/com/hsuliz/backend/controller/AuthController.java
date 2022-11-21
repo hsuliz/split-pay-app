6f1de996ad1d64d70f397ae9b615d1f67f8609c1
mymytre <sasha.suliz@gmail.com>
2022-11-21 12:36:03 +0100

    Split to diff classes

--- back-end/src/main/java/com/hsuliz/backend/controller/AuthController.java
+++ back-end/src/main/java/com/hsuliz/backend/controller/AuthController.java
@@ -1,16 +1,10 @@
 package com.hsuliz.backend.controller;
 
-import com.hsuliz.backend.entity.Client;
 import com.hsuliz.backend.model.LoginRequest;
-import com.hsuliz.backend.repository.ClientRepository;
-import com.hsuliz.backend.service.TokenService;
+import com.hsuliz.backend.service.ClientAuthService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,34 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AuthController {
 
-    private final ClientRepository clientRepository;
-
-    private final TokenService tokenService;
-
-    private final AuthenticationManager authenticationManager;
-
-    private final PasswordEncoder passwordEncoder;
-
+    private final ClientAuthService clientAuthService;
 
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest clientLogin) throws AuthenticationException {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(
-                        clientLogin.username(),
-                        clientLogin.password()));
-        log.info("Access granted!!");
-        return tokenService.generateToken(authentication);
+        return clientAuthService.authenticate(clientLogin);
     }
 
     @PostMapping("/register")
     public String register(@RequestBody LoginRequest userLogin) throws AuthenticationException {
-        String encodedPass = passwordEncoder.encode(userLogin.password());
-        var client = new Client(
-                userLogin.username(),
-                encodedPass
-        );
-        clientRepository.save(client);
-        return "User created!!";
+        return clientAuthService.register(userLogin);
     }
 
 }
---
+++ back-end/src/main/java/com/hsuliz/backend/service/ClientAuthService.java
@@ -0,0 +1,46 @@
+package com.hsuliz.backend.service;
+
+import com.hsuliz.backend.entity.Client;
+import com.hsuliz.backend.model.LoginRequest;
+import com.hsuliz.backend.repository.ClientRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class ClientAuthService {
+
+    private final ClientRepository clientRepository;
+
+    private final TokenService tokenService;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public String authenticate(LoginRequest clientLogin) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        clientLogin.username(),
+                        clientLogin.password()));
+        log.info("Access granted!!");
+        return tokenService.generateToken(authentication);
+    }
+
+    public String register(LoginRequest userLogin) {
+        String encodedPass = passwordEncoder.encode(userLogin.password());
+        var client = new Client(
+                userLogin.username(),
+                encodedPass
+        );
+        clientRepository.save(client);
+        return "User created!!";
+    }
+
+}
