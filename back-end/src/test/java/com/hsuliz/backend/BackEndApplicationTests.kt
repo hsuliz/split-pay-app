62c4f4433fea08089fc4fdc31c17cbcef433e63b
mymytre <sasha.suliz@gmail.com>
2022-11-22 12:42:43 +0100

    Serializable login to client

--- back-end/src/test/java/com/hsuliz/backend/BackEndApplicationTests.kt
+++ back-end/src/test/java/com/hsuliz/backend/BackEndApplicationTests.kt
@@ -1,30 +1,40 @@
 package com.hsuliz.backend
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.hsuliz.backend.model.LoginRequest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
+import java.util.*
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 internal class BackEndApplicationTests {
 
     @LocalServerPort
-    private var port: Int? = null
-
-    private var baseUrl: String = "http://localhost"
+    private var port: Int = 0
 
     @Autowired
     private lateinit var h2Repository: TestH2Repository
 
+    private var baseUrl: String = "http://localhost"
+
 
     companion object {
 
+        private val client = LoginRequest("sasha", "password")
+
         private lateinit var restTemplate: RestTemplate
+
+        private lateinit var accessToken: String
 
         @JvmStatic
         @BeforeAll
@@ -37,26 +47,54 @@ internal class BackEndApplicationTests {
     //http://localhost:8080/api/auth/register
     @BeforeEach
     fun beforeEach() {
-        baseUrl = "$baseUrl:$port/api/auth"
+        baseUrl = "$baseUrl:$port/api"
     }
 
 
     @Test
-    fun test() {
-        // given
-        val client = LoginRequest("sasha", "password")
-        val response = restTemplate.postForEntity(
-            "$baseUrl/register",
-            client,
-            String::class.java
+    @Order(1)
+    fun `given client should register`() {
+        // given, when
+        val responseFromRegister = restTemplate.postForEntity(
+            "$baseUrl/auth/register", client, String::class.java
         )
 
-
-
-
-        assertThat(response.statusCode.is2xxSuccessful).isTrue
-        assertThat(h2Repository.findAll().size).isEqualTo(1)
+        // then
+        assertAll(
+            { assertThat(responseFromRegister.statusCode.is2xxSuccessful).isTrue },
+            { assertThat(h2Repository.findAll().size).isEqualTo(1) }
+        )
     }
 
+    @Test
+    @Order(2)
+    fun `given client should login`() {
+        // given, when
+        val responseFromLogin = restTemplate.postForEntity(
+            "$baseUrl/auth/login", client, String::class.java
+        )
+
+        // then
+        assertThat(responseFromLogin.statusCode.is2xxSuccessful).isTrue
+        accessToken = responseFromLogin.body as String
+    }
+
+    @Test
+    @Order(3)
+    fun `given client should get details`() {
+        // given
+        val mapper = jacksonObjectMapper()
+        val headers = HttpHeaders()
+        headers.set("Authorization", "Bearer $accessToken")
+        val entity = HttpEntity("body", headers)
+
+        // when
+        val responseFromInfo = restTemplate.exchange(
+            "$baseUrl/clients", HttpMethod.GET, entity, String::class.java
+        )
+
+        // then
+        val x = responseFromInfo.body?.let { mapper.readValue<LoginRequest>(it) }
+    }
 
 }
